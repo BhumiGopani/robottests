8e69f1e08942c3cf57359f968d5c707136601f3c
gopani-bhumi <gopani@architrave.de>
2020-06-18 17:32:23 +0200

    intial commit

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -3,14 +3,14 @@
 set -euxo pipefail
 
 # Clean previous test results
-#sudo rm -rf reports/
-#mkdir -m 777 reports
+sudo rm -rf tests/reports/
+mkdir -m 777 tests/reports/
 
 GID=`id -g`
 
 # Execute e2e using docker
 docker run --rm \
-    -v `pwd`/:/opt/robotframework/reports:Z \
+    -v `pwd`/:/opt/robotframework/tests/reports/:Z \
     -v `pwd`/:/opt/robotframework/tests:Z \
     -v /dev/shm:/dev/shm \
     -e ROBOT_THREADS=1 \
